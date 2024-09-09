7b98f4cd46a8a51070bd84c6f1056a46c5026104
Dilemma98 <emmaah98@outlook.com>
2024-09-09 16:50:36 +0200

    Funktionen på plats

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ let p2 = document.createElement("p");
 
 const usernameInput = document.createElement("input");
 usernameInput.type = "text";
-usernameInput.id = "username";
+
+const userName = "test";
+const userPassword = "1234";
 
 const passwordInput = document.createElement("input");
 passwordInput.type = "password";
-passwordInput.id = "password";
-
 
 p.innerText ="Användarnamn:";
 p.id = "anvNamn";
@@ -21,11 +21,18 @@ p2.innerText="Lösenord:";
 p2.id = "anvLösen";
 
 h2.innerText = "Varsågod att logga in.";
-inputForm.appendChild(h2);
+root.appendChild(h2);
 inputForm.appendChild(p);
 inputForm.appendChild(usernameInput);
 inputForm.appendChild(p2);
 inputForm.appendChild(passwordInput);
 inputForm.appendChild(button);
 
-
+button.addEventListener("click", () => {
+    let result = usernameInput.value;
+    let result2 = passwordInput.value;
+    if (result == userName && result2 == userPassword){
+     h2.innerHTML ="Välkommen in!"
+     inputForm = " ";
+    }
+})
