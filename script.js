4df3515c7221ea7176c43fecab7f44b269f1ae80
Dilemma98 <emmaah98@outlook.com>
2024-09-10 10:58:44 +0200

    Fixade till så användaren kunde logga in igen med hjälp av en ny ifsats utanför den jag redan hade

--- script.js
+++ script.js
@@ -3,75 +3,58 @@ let root = document.getElementById("root");
 let inputField = document.getElementById("inputField");
 let button = document.createElement("button");
 button.textContent = "Logga in";
-let p = document.createElement("p");
-let p2 = document.createElement("p");
-
+const h3 = document.createElement("h3");
+h3.innerText = "Välkomna Janne och Asaf!";
 let usernameInput = document.createElement("input");
 usernameInput.type = "text";
 usernameInput.placeholder = "Användarnamn:";
-
 let userName = "test";
 let userPassword = "1234";
-
 let passwordInput = document.createElement("input");
 passwordInput.type = "password";
-passwordInput.placeholder ="Lösenord:";
-
-/*const checkUser = localStorage.getItem("username");
-if (checkUser){
-  h2.innerHTML = "Välkommen in!";
-    inputField.removeChild(usernameInput);
-    inputField.removeChild(passwordInput);
-    inputField.removeChild(p);
-    inputField.removeChild(p2);
-    button.textContent = "Logga ut";
-}*/
+passwordInput.placeholder = "Lösenord:";
 
 h2.innerText = "Varsågod att logga in.";
 
-inputField.appendChild(p);
 inputField.appendChild(usernameInput);
-inputField.appendChild(p2);
 inputField.appendChild(passwordInput);
 inputField.appendChild(button);
 
 root.appendChild(inputField);
 root.insertBefore(h2, inputField);
 
-//LOGGA IN--------------------------------------------
+let loggedIn = false;
+
 button.addEventListener("click", () => {
-  let result = usernameInput.value;
-  let result2 = passwordInput.value;
+  if (!loggedIn) {
+    // Logga in
+    let result = usernameInput.value;
+    let result2 = passwordInput.value;
 
-  if (result == userName && result2 == userPassword) {
-    localStorage.setItem("username", userName);
-    h2.innerHTML = "Välkommen in!";
-    inputField.removeChild(usernameInput);
-    inputField.removeChild(passwordInput);
-    inputField.removeChild(p);
-    inputField.removeChild(p2);
-    button.textContent = "Logga ut";
-
-    //LOGGA UT------------------------------------------
-    button.addEventListener("click", () => {
-      localStorage.clear();
-      h2.innerText = "Varsågod att logga in.";
-      
-      inputField.appendChild(p);
-      inputField.appendChild(usernameInput);
-      inputField.appendChild(p2);
-      inputField.appendChild(passwordInput);
-      inputField.appendChild(button);
-
-      root.appendChild(inputField);
-      root.insertBefore(h2, inputField);
+    if (result == userName && result2 == userPassword) {
+      localStorage.setItem("username", userName);
+      h2.innerHTML = "Välkommen in!";
+      inputField.removeChild(usernameInput);
+      inputField.removeChild(passwordInput);
+      button.textContent = "Logga ut";
+      document.body.appendChild(h3);
+      loggedIn = true;
+    } else {
+      h2.innerHTML = "Användarnamn eller lösenord matchade inte. Försök igen";
       usernameInput.value = "";
       passwordInput.value = "";
-      button.textContent = "Logga in";
-    });
-
+    }
   } else {
-    h2.innerHTML = "Användarnamn eller lösenord matchade inte. Försök igen";
+    // Logga ut
+    localStorage.clear();
+    document.body.removeChild(h3);
+    h2.innerText = "Varsågod att logga in.";
+
+    inputField.appendChild(usernameInput);
+    inputField.appendChild(passwordInput);
+    inputField.appendChild(button);
+    button.textContent = "Logga in";
+    loggedIn = false;
     usernameInput.value = "";
     passwordInput.value = "";
   }
